f6dfb02bde1dc37ae47f170144a0c7cd8c713a50
yingfeng ni <lovenyf@163.com>
2013-01-08 17:25:21 +0800

    注释

--- tools.js
+++ tools.js
@@ -1,6 +1,6 @@
 window["_BFD"] = window["_BFD"] || {};
 
-_BFD.del_same = (function(){// 去除对象中特定属性值相同的对象
+_BFD.del_same = (function(){// 去除对象中特定属性值相同的商品对象
 
 	var iname = {};//闭包数据
 
@@ -52,7 +52,7 @@ _BFD.del_same = (function(){// 去除对象中特定属性值相同的对象
 _BFD.del_same(arr_obj,prop, *num*)   商品对象信息数组（必须）  去重的商品对象属性（必须） 所需的数量（可选）
  
 var a = [{iid:"111",name:"it is a1",g_id:["1","4"]},{iid:"112",name:"it is a2",g_id:["2"]},{iid:"113",name:"it is a3",g_id:["3"]}];
-var b = [{iid:"111",name:"it is b1",g_id:["6"]},{iid:"114",name:"it is b2",g_id:["4"]},{iid:"115",name:"it is a3",g_id:["1"]}];
+var b = [{iid:"111",name:"it is b1",g_id:["6"]},{iid:"114",name:"it is b2",g_id:["4"]},{iid:"115",name:"it is a3",g_id:["1"]}]; 
 
 var c = _BFD.del_same(a,"iid",2); //[{iid:"111",name:"it is a1",g_id:["1","4"]},{iid:"112",name:"it is a2",g_id:["2"]}]
 var d = _BFD.del_same(b,"iid"); //[{iid:"114",name:"it is b2",g_id:["4"]},{iid:"115",name:"it is a3",g_id:["1"]}]
