12c8355f7ba01c692f7c7ac472a4224f18cfe6fd
yingfeng ni <lovenyf@163.com>
2013-01-09 19:42:28 +0800

    feedback事件绑定

--- tools.js
+++ tools.js
@@ -66,3 +66,38 @@ var h = _BFD.del_same(b,"g_id"); // [{iid:"111",name:"it is b1",g_id:["6"]}];
 */
 
 
+
+
+_BFD.try_turn = function(str,arr_id,req_id,banner_id){//执行该方法需要有 可以获取节点的方法 $ 方法
+	
+	if($(str).length > 0){
+		var arr_dom = $(str);
+		_BFD.hand_bfd_event(arr_dom,arr_id,req_id,banner_id);
+	}else{
+		//arguments.callee(str,fn,arr_id,req_id,banner_id);
+		setTimeout(function(){_BFD.try_turn(str,arr_id,req_id,banner_id);},100);
+	}
+
+}
+
+_BFD.hand_bfd_event = function(arr,arr_id,req_id,banner_id){// 商品节点对象数组  对于节点商品id req_id banner_id
+	var i=0,j,len = arr.length,_a,_a_len,bfd_tools = BCore.tools.Tools;
+	for(;i<len;i++){
+		_a = (arr[i]).getElementsByTagName("a");
+		_a_len = _a.length;
+		for(j = 0;j<_a_len;j++){
+			bfd_tools.bind(_a[j],"click",(function(x,iid){
+				return function (){
+					var _this = new BCore();
+					var fb = new BCore.inputs.FeedBack(req_id);
+					fb.p_on = x + 1;
+					fb.p_bid = banner_id;
+					fb.iid = iid;
+					_this.send(fb);
+					
+				}
+			})(i, arr_id[i]));
+		}
+	}
+}
+
