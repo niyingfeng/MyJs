02c1c139231a5fd343b73810487d6e4b00edb133
lovenyf <lovenyf@163.com>
2014-05-11 22:58:08 +0800

    添加模板引擎库

---
+++ template.js
@@ -0,0 +1,58 @@
+(function(){
+    var escapeMap = {
+            '&' : '&amp;',
+            '<' : '&lt;',
+            '>' : '&gt;',
+            '"' : '&quot;',
+            "'" : '&#x27;'
+        },
+
+        escapeReg = /[&<>"\']/g,
+
+        transformMethod = {
+            escape : '<%=([\\s\\S]+?)%>',
+            unescape : '<%-([\\s\\S]+?)%>',
+            normal : '<%([\\s\\S]+?)%>'
+        },
+        transformReg = new RegExp([
+            transformMethod.escape,
+            transformMethod.unescape,
+            transformMethod.normal].join('|'), 'g');
+
+    var esacpeFunc = function( str ){
+        return str.replace( escapeReg, function( match ){
+            return escapeMap[ match ];
+        } );
+    };
+
+
+    return function( tmpl, data ){
+        var tmplStr = "_s+='",
+            index = 0, len = tmpl.length;
+
+        var c = tmpl.replace( transformReg, 'a');
+        tmpl.replace( transformReg, function( match, escape, unescape, normal, offset){
+
+            tmplStr += tmpl.slice(index, offset);
+
+            if( escape ){
+                tmplStr += "';\n_s+=((" + escape + ")==null?'':esacpeFunc(" + escape + "));\n_s+='"
+            }else if( unescape ){
+                tmplStr += "';\n_s+=((" + unescape + ")==null?'':" + unescape + ");\n_s+='"
+            }else if( normal ){
+                tmplStr += "';\n" + normal + ";\n_s+='"
+            }
+
+            index = offset + match.length;
+        } );
+
+        tmplStr += tmpl.slice(index, len) + "'";
+
+        tmplStr = "var _s='';\n with(data){\n "+ tmplStr +"}\n return _s;";
+
+        var tmplFunc = new Function( "data", tmplStr );
+
+        return data ? tmplFunc( data ) : tmplFunc;
+    }
+
+})();
