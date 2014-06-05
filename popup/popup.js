ab52c5ef30b4361639e44e7439430afd09459bb8
lovenyf <lovenyf@163.com>
2014-06-05 23:41:37 +0800

    开始编写弹窗插件

---
+++ popup/popup.js
@@ -0,0 +1,11 @@
+// popup 模拟windows弹窗3种状态 以及自定义状态
+
+(function( factory ){
+	if( typeof define === "function" && define.amd ){
+		define( ["jquery"], factory );
+	}else{
+		factory( jquery );
+	}
+}(function( $ ){
+
+}));
