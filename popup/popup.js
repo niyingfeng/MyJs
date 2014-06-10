1e65b2556b63cf1b3eeb041c9c7d2a4b858d5536
lovenyf <lovenyf@163.com>
2014-06-10 20:12:01 +0800

    进行中

--- popup/popup.js
+++ popup/popup.js
@@ -4,13 +4,19 @@
 
 (function( factory ){
 	if( typeof define === "function" && define.amd ){
-		define( ["jquery"], factory );
+		define( ["jQuery"], factory );
 	}else{
-		factory( jquery );
+		factory( jQuery );
 	}
 }(function( $ ){
+	'use strict';
 
-	var HTMLTmpl = {
+	var popOverScreenSelector = '.popup-overScreen',
+		popContainerSelector = '.popup-container',
+
+
+
+		HTMLTmpl = {
 			overScreen : "<div class='popup-overScreen'></div>",  // 外层overlay
 			container : "<div class='popup-container'>" +	// 内层弹窗主体
 								"<div clas='popup-header'>{% header %}</div>" +
@@ -19,15 +25,13 @@
 							"</div>",
 			header : "<h3 class='popup-title'>{% title %}</h3><span class='popup-close'>X</span>",
 			body : "<p class='popup-content'>{% content %}</p>",
-			promptBody : "<p class='popup-content'>{% content %}</p><input type='text' class='popup-publish' />",
-			ensureBtnHtml : "<span></span>"
-		},
-		
-		$overScreen = $( overHtml ),
-		$body = $("body");
+			promptInput : "<input type='text' class='popup-publish' />",
+			ensureBtnHtml : "<span class='popup-ensure'>确定</span>",
+			cancelBtnHtml : "<span class='popup-cancel'>取消</span>"
+		};
 
 	// 无需转义 简单替换方式即可
-	function fomart( template, data ){
+	function transform( template, data ){
 		return template.replace( /\{%(?:\s*?(\S\S*?)\s*?)%\}/g, function(match, key){
 			return data[key] || "";
 		} )
@@ -49,41 +53,75 @@
     *   @return {Object} 返回目标对象
     *   
     */
-	function popup( type ,argsObj ){
+	function PopUp( argsObj ){
 
-		var popType = type,
+		// this.type 
+		// this.title 
+		// this.width 
+		// this.height 
 
-			title = argsObj.title || "来自系统的消息",
-			width = argsObj.width || 500,
-			height = argsObj.height || 300;
-
-
-
-
+		this.init( argsObj );
 	}
 
-	popup.prototype = {
+	PopUp.prototype = {
 		constructor : popup,
 
-		_appendPopup : function(){},
+		init : function( argsObj ){
+			this.type = argsObj.type;
+			this.title = argsObj.title || "来自系统的消息";
+			this.width = argsObj.width || 500;
+			this.height = argsObj.height || 300;
+
+			this.$element = $( HTMLTmpl.overScreen );
+
+			this._rander();
+			this._appendPopup();
+		},
+
+		_rander : function(){
+			var self = this,
+				partHtmlObj = {
+					header : HTMLTmpl.header,
+					body : HTMLTmpl.body,
+					footer : HTMLTmpl.ensureBtnHtml
+				};
+
+			switch( this.type ){
+				case "prompt" : 
+					partHtml.body = HTMLTmpl.body + HTMLTmpl.promptInput;
+
+				case "confirm" : 
+					partHtml.footer = HTMLTmpl.ensureBtnHtml + HTMLTmpl.cancelBtnHtml;
+
+				case "alert" : 
+
+			}
+
+			this.$element.html( transform( HTMLTmpl.container, partHtmlObj ) );
+			this.$element.find( popContainerSelector ).css({
+				width : self.width,
+				height : self.height
+			})
+
+		},
+
+		_appendPopup : function(){
+
+			$("body").append( this.$element );
+
+		},
 		_removePopup : function(){},
 
 		_ensurePopup : function(){},
 		_canclePopup : function(){},
 
+	}
 
-		// alert : function(){
-		// 	$overScreen.html(containerHtml);
 
-		// 	$body.append( $overScreen );
-
-		// },
-
-		// confirm : function(){},
-
-		// prompt : function(){},
-
-		// overlay : function(){}
+	function popup( type, obj ){
+		obj = obj || {};
+		obj.type = type;
+		return new PopUp( obj );
 	}
 
 
@@ -91,7 +129,7 @@
 	var old = $.popup;
 	$.popup = popup;
 
-	$.popup.noConflict : function(){
+	$.popup.noConflict = function(){
 		$.popup = old;
 		return popup;
 	}
